915c60ad1388bc6b8a8493d4c248d330b60a818d
adelazalewski <paliuadela@yahoo.com>
2020-08-25 18:48:13 -0400

    created 2 components and added my axios to my files also got the data request response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-
+//https://api.nasa.gov/planetary/apod
 function App() {
+
+  
+ useEffect(() => {
+axios
+ .get(`https://api.nasa.gov/planetary/apod?api_key=KHfBOCneFgtNxSmW61NFFecwj3haO07gOURbCyGB`)
+ .then((res) => {
+   console.log('this is data from API: ', res);
+ })
+ .catch((err) => {
+   console.log('this is an error: ', err);
+ })
+}, []);
   return (
     <div className="App">
       <p>
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <header>
+                <img id="nasa-logo" alt="nasa-logo" src="src\components\1224px-NASA_logo.svg.png"></img>
+                <h1>NASA's Photo Of The Day App</h1>
+                <h2>By Adela Zalewski</h2>
+                <img id=""
+            </header>
+        </div>
+    )
+}
+
+export default Header;
